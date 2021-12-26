03715043c0b08c01497f4cb62c76731571dc1868
MAX <debaev@list.ru>
2021-12-26 18:54:33 +0500

    feat: add main page and style to it

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,1 +1,31 @@
 import './style.css';
+
+
+
+
+
+
+
+
+
+
+// import Home             from './assets/views/pages/Home'
+// import ToysPage         from './assets/views/pages/ToysPage'
+// import PlayGroundPage   from './assets/views/pages/Pictures'
+
+// const mainContent = document.querySelector('.main') as HTMLElement;
+
+// const routes = {
+//       '/'           : Home
+//     , '/toys'       : ToysPage
+//     , '/tree'       : PlayGroundPage
+//     , '/home'       : Home
+// };
+// const router = async () => {
+//     const parsedURL:string = '/' + location.hash.slice(1).toLowerCase();
+//     const page = routes[parsedURL] ? routes[parsedURL] : Error404
+//     mainContent.innerHTML = await page.render();
+//     await page.after_render();
+// }
+// window.addEventListener('hashchange', router);
+// window.addEventListener('load', router);
